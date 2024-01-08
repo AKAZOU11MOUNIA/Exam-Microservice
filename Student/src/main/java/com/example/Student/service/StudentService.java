ab07db3ae683ae8c9c8620e7f1adc88f6562eb5f
AKAZOU_Mounia <mounia.akazou@edu.uca.ma>
2024-01-08 10:41:38 +0100

    finalisation du crud student

--- Student/src/main/java/com/example/Student/service/StudentService.java
+++ Student/src/main/java/com/example/Student/service/StudentService.java
@@ -6,47 +6,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
 @Service
 public class StudentService {
-@Autowired
+
+    @Autowired
     private StudentRepository studentRepository;
+
     // Get all students
-    public Iterable<Student> getAll(){
+    public Iterable<Student> getAll() {
         return studentRepository.findAll();
     }
+
     // Get a student by ID
-    public Optional<Student> getSpecificStudent(@PathVariable("id") final Long id){
+    public Optional<Student> getSpecificStudent(Long id) {
         return studentRepository.findById(id);
     }
-    //Delete a Student
-    public ResponseEntity<String> deleteStudent(@PathVariable("id") final Long id){
+
+    // Delete a Student
+    public ResponseEntity<String> deleteStudent(Long id) {
         try {
             studentRepository.deleteById(id);
-            return ResponseEntity.ok("l'etudiant est supprimé avec succès");
+            return ResponseEntity.ok("L'étudiant a été supprimé avec succès");
         } catch (Exception e) {
             // Log l'erreur pour le débogage
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Une erreur s'est produite lors de la suppression de l'etudiant.");
+                    .body("Une erreur s'est produite lors de la suppression de l'étudiant.");
         }
-        //Add a student
-        public Student addNewStudent(Student s) {
-            return studentRepository.save(s);
-        }
-        //update a student
-        public ResponseEntity<String> updateStudent( Long id,Student s){
-            Optional<Student> existingStudentOptional= studentRepository.findById(id);
-            if(existingStudentOptional.isPresent()){
-                Student existingStudent = existingStudentOptional.get();
-                if (s.getNom!=null) existingStudent.setNom(s.getNom());
-                if (s.getPrenom!=null) existingStudent.setPrenom(s.getPrenom());
-                if (s.getCne!=null) existingStudent.setCne(s.getCne());
-                if (s.getEmail!=null) existingStudent.setEmail(s.getEmail());
-            }
+    }
+
+    // Add a student
+    public Student addNewStudent(Student s) {
+        return studentRepository.save(s);
+    }
+
+    // Update a student
+    public ResponseEntity<String> updateStudent(Long id, Student s) {
+        Optional<Student> existingStudentOptional = studentRepository.findById(id);
+        if (existingStudentOptional.isPresent()) {
+            Student existingStudent = existingStudentOptional.get();
+            if (s.getNom() != null) existingStudent.setNom(s.getNom());
+            if (s.getPrenom() != null) existingStudent.setPrenom(s.getPrenom());
+            if (s.getCne() != null) existingStudent.setCne(s.getCne());
+            if (s.getEmail() != null) existingStudent.setEmail(s.getEmail());
+
+            studentRepository.save(existingStudent); // Sauvegarder les modifications
+            return ResponseEntity.ok("L'étudiant a été mis à jour avec succès");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("L'étudiant avec l'ID spécifié n'a pas été trouvé.");
         }
     }
 }
